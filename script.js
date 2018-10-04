74e13a78b43590e514fc047b9790b913dd764b65
Your Name <you@example.com>
2018-10-04 11:05:50 +0200

    Add button which toggles bg

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 
-console.log("It works!");
+
+function changeBackground() {
+    
+    var style = document.body.style;
+
+    if (document.body.style.backgroundColor == "red") {
+        document.body.style.backgroundColor = "white";
+    } else {
+        document.body.style.backgroundColor = "red";
+    }
+
+    console.log("It works!");
+}
+
